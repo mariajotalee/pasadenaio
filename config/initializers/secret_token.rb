ae2e8b94a25fc5c7ebca279d1d530bf988c55395
Maria Lee <lcmarialee@gmail.com>
2014-03-29 15:54:29 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pasadenaio::Application.config.secret_key_base = '1e5048583d5242c807ee0170aee3290b55f3fca37e3a1a337ee794ce437aa33e7237393280e93f6e18057e16cf83656fac99fb522e5caa8c8bbdf51f60c18b04'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pasadenaio::Application.config.session_store :cookie_store, key: '_pasadenaio_session'
